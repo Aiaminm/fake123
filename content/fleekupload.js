b37b603c0c608f6d26e2a9ef2c456ae68e0d5660
Aiaminm <70017178+Aiaminm@users.noreply.github.com>
2022-06-20 18:33:59 +0800

    Add files via upload

---
+++ content/fleekupload.js
@@ -0,0 +1,27 @@
+let input=process.argv
+const fleek = require('@fleekhq/fleek-storage-js');   
+var fs = require("fs");
+const path = require('path');
+
+var pathre=path.resolve(input[2])
+
+
+upLoad(pathre)
+
+
+
+async function upLoad(filename){
+
+
+const upinput = {
+    apiKey: input.fleek_api_token,
+    apiSecret: input.fleek_api_secret,
+    key: `upload/`+path.basename(filename),
+    stream: fs.createReadStream(filename),
+  };
+  console.log('开始上传 '+filename)
+  const result = await fleek.streamUpload(upinput);
+  console.log(result)
+}
+
+
