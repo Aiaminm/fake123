3dde7baf134936258ccc5a914cd26411e9081faf
Aiaminm <70017178+Aiaminm@users.noreply.github.com>
2022-06-21 22:41:55 +0800

    Add files via upload

--- content/fleekupload.js
+++ content/fleekupload.js
@@ -11,25 +11,59 @@ upLoad(pathre)
 
 
 async function upLoad(filename){
-var i=0
-var j=1
-while(i==0&&j<4){
-try {
+  try {
+    var isDirectory=fs.statSync(filename).isDirectory()
+    if(isDirectory===true){
+      console.log(filename+"是文件夹")
+    }else if(isDirectory===false){
+      console.log(filename+"是文件")
+    }else{
+      console.log('文件选择有问题')
+      return
+    }
+  } catch (error) {
+      console.error('没有文件或path参数有问题')
+      return
+  }
+
+
+
+  if(isDirectory===true){
+    var filelist=fs.readdirSync(filename)
+  //  console.log(filelist)
+    
+    for(var i in filelist){
+      console.log(filelist[i])
+      var forpath =path.resolve(filename,filelist[i])
+      console.log(forpath)
+      
+      if(fs.statSync(forpath).isDirectory()==true){
+        console.log(forpath+' 是嵌套文件夹，不支持');
+        continue;
+      }else{
+      await goUp(forpath)}
+        
+    }
+  }else if(isDirectory===false){
+  
+    await goUp(filename)
+  }
+
+
+
+
+}
+
+
+async function goUp(forpath){
 
 const upinput = {
     apiKey: process.env.fleek_api_token,
     apiSecret: process.env.fleek_api_secret,
-    key: `upload/`+path.basename(filename),
-    stream: fs.createReadStream(filename),
+    key: `upload/`+path.basename(forpath),
+    stream: fs.createReadStream(forpath),
   };
-  console.log('开始上传 '+filename)
+  console.log('开始上传 '+forpath)
   const result = await fleek.streamUpload(upinput);
   console.log(result)
-  i++
-}
-catch (error) {
-console.error(path.basename(filename)+'上传失败 '+j)
-j++
-}
-}
 }
